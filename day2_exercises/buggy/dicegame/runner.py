0df2cdb30072655abfd21a16912eb651812df6cd
peng.m <peng.man@uu.se>
2026-03-17 16:13:48 +0100

    refactor variable c as game_round

--- day2_exercises/buggy/dicegame/runner.py
+++ day2_exercises/buggy/dicegame/runner.py
@@ -22,7 +22,7 @@ class GameRunner:
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
+        game_round = 0
         while True:
             runner = cls()
 
@@ -38,17 +38,17 @@ class GameRunner:
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
-                c += 1
+                game_round += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
                 runner.loses += 1
-                c = 0
+                game_round = 0
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
             runner.round += 1
 
-            if c == 6:
+            if game_round == 6:
                 print("You won... Congrats...")
                 print("The fact it took you so long is pretty sad")
                 break
