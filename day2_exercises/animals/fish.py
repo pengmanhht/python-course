0b94e0cc0dc8e0241478aebc3a7d213e28e51265
peng.m <peng.man@uu.se>
2026-03-17 14:59:07 +0100

    add Fish class

---
+++ day2_exercises/animals/fish.py
@@ -0,0 +1,15 @@
+
+class Fish:
+    def __init__(self):
+        self.members = ["Tuna", "Jellyfish", "Lionfish"]
+
+    def print_members(self):
+        print("printing members of the Fish class")
+        for member in self.members:
+            print(f"\t{member}")
+
+
+
+if __name__ == "__main__":
+    fish = Fish()
+    fish.print_members()
