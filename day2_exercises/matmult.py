33a788fe383b16aeecd9d531611b65d420dff849
peng.m <peng.man@uu.se>
2026-03-17 20:10:41 +0100

    improve matmult using numpy

--- day2_exercises/matmult.py
+++ day2_exercises/matmult.py
@@ -1,30 +1,39 @@
 # Program to multiply two matrices using nested loops
-import random
+# import random
+import numpy as np
 
 N = 250
 
 # NxN matrix
-X = []
-for i in range(N):
-    X.append([random.randint(0,100) for r in range(N)])
+# X = []
+# Y = []
+# result = []
 
+# for i in range(N):
+# X.append([random.randint(0,100) for r in range(N)])
+# Y.append([random.randint(0,100) for r in range(N+1)])
+    # result.append([0] * (N+1))
+
+X = np.random.randint(0, 100, size=(N, N))
+Y = np.random.randint(0, 100, size=(N, N+1))
+result = np.matmul(X, Y)
 # Nx(N+1) matrix
-Y = []
-for i in range(N):
-    Y.append([random.randint(0,100) for r in range(N+1)])
+# Y = []
+# for i in range(N):
+# Y.append([random.randint(0,100) for r in range(N+1)])
 
 # result is Nx(N+1)
-result = []
-for i in range(N):
-    result.append([0] * (N+1))
+# result = []
+# for i in range(N):
+# result.append([0] * (N+1))
 
 # iterate through rows of X
-for i in range(len(X)):
+# for i in range(len(X)):
     # iterate through columns of Y
-    for j in range(len(Y[0])):
+#     for j in range(len(Y[0])):
         # iterate through rows of Y
-        for k in range(len(Y)):
-            result[i][j] += X[i][k] * Y[k][j]
+#         for k in range(len(Y)):
+#             result[i][j] += X[i][k] * Y[k][j]
 
 # for r in result:
     # print(r)
