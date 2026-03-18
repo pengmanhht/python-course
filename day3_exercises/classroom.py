f00d91d127f5ad301e6ee3b31ef6a5f8d210ad2f
peng.m <peng.man@uu.se>
2026-03-18 13:58:54 +0100

    add Student class

--- day3_exercises/classroom.py
+++ day3_exercises/classroom.py
@@ -7,3 +7,19 @@ class Person:
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
         
+
+
+class Student(Person):
+
+    def __init__(self, first_name, last_name, subject):
+        super().__init__(first_name, last_name)
+        self.subject = subject
+
+    def print_name_subject(self):
+        full_name = self.get_full_name()
+        print(f"Name: {full_name}, Subject: {self.subject}")
+
+
+if __name__ == "__main__":
+    s = Student("P", "M", "Medicine")
+    s.print_name_subject()
