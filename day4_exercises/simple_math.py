a2c68b2e7a9661fcb8499aeecedc29d9570a47a3
peng.m <peng.man@uu.se>
2026-03-22 13:44:28 +0100

    add docstrings for simple_add and simple_sub

--- day4_exercises/simple_math.py
+++ day4_exercises/simple_math.py
@@ -3,9 +3,39 @@ A collection of simple math operations
 """
 
 def simple_add(a,b):
+    """
+    adds two numbers together and returns the result
+
+    Parameters
+    ----------
+    a : int or float
+        The first number to add
+    b : int or float
+        The second number to add
+    Returns
+    -------
+    int or float
+        The sum of a and b
+    """
     return a+b
 
 def simple_sub(a,b):
+    """
+    subtracts the second number from the first
+
+    Parameters
+    ----------
+    a : int or float
+        The number to subtract from
+    b : int or float
+        The number to subtract
+
+    Returns
+    -------
+    int or float
+        The difference of a and b
+    """
+
     return a-b
 
 def simple_mult(a,b):
